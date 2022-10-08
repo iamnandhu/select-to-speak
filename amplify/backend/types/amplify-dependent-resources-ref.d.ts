27973c950bfaadc1c5d5488da2d37d2e08736639
Nandhu S <nandhus1810@gmail.com>
2022-10-08 22:12:03 +0530

    initial commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,29 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "nlpchromeextension1e5d9212": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "predictions": {
+        "interpretText6483226e": {
+            "region": "string",
+            "type": "string"
+        },
+        "translateTextaa15511f": {
+            "region": "string",
+            "sourceLang": "string",
+            "targetLang": "string"
+        },
+        "speechGenerator5d65d42f": {
+            "region": "string",
+            "language": "string",
+            "voice": "string"
+        }
+    }
+}
