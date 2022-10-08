fa0f8733679eb1e9719bed69d91d3d27f934b885
Nandhu S <nandhus1810@gmail.com>
2022-10-08 22:16:12 +0530

    Update popup

---
+++ src/js/popup.js
@@ -0,0 +1,17 @@
+import "../css/popup.css";
+
+window.addEventListener('DOMContentLoaded', () => {
+  var buttons = document.getElementsByClassName("lang-button");
+  Array.from(buttons).forEach(function(button) {
+    button.addEventListener('click', function(item) {
+      Array.from(buttons).forEach(item => item.classList.remove("button-selected"))
+      item.target.classList.add("button-selected")
+      const language = item.target.dataset.id
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {language}, function(response) {
+          console.log('response: ', response)
+        });
+      });
+    });
+  });
+});
